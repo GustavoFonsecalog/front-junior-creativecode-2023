3dc90f6e2d4e38aef8e250456697bd2bb9436d39
GustavoFonsecalog <gustavofonsecajb@hotmail.com>
2023-04-28 14:59:41 -0300

    Adjusting navbar

--- cc-system/src/components/organisms/navbar.tsx
+++ cc-system/src/components/organisms/navbar.tsx
@@ -2,12 +2,22 @@ import * as React from 'react';
 import { AppBar, Button, Box, Toolbar, Typography, IconButton, Grid } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 
+import { Link } from 'react-router-dom'
+
+import Logo from '../../assets/images/logo.png'
+import styled from '@emotion/styled';
+
+const LogoImg = styled("img")({
+    maxWidth: '120px',
+    maxHeiht: '150px',
+})
+
 export default function ButtonAppBar() {
     return (
         <Grid container>
             <Grid item lg={12} md={12} xs={12}>
                 <Box sx={{ flexGrow: 1 }}>
-                    <AppBar position="static">
+                    <AppBar position="static" sx={{ backgroundColor: '#620cca' }}>
                         <Toolbar>
                             <IconButton
                                 size="large"
@@ -16,12 +26,19 @@ export default function ButtonAppBar() {
                                 aria-label="menu"
                                 sx={{ mr: 2 }}
                             >
-                                <MenuIcon />
+                                <MenuIcon sx={{ color: '#000' }} />
                             </IconButton>
-                            <Typography variant="h6" component="div" sx={{ flexGrow: 1, justifyContent: 'start' }}>
-                                News
+                            <Typography variant="h6" component="div" sx={{ flexGrow: 1, marginLeft: '100px' }}>
+                                <Link to={'/'}>
+                                    <LogoImg src={Logo} />
+                                </Link>
                             </Typography>
-                            <Button color="inherit">Login</Button>
+                            <Link to={'/private'}>
+                                <Button sx={{ color: '#000', fontWeight: 'bold' }}> Contatos </Button>
+                            </Link>
+                            <Link to={'/login'}>
+                                <Button sx={{ color: '#000', fontWeight: 'bold' }}>Login</Button>
+                            </Link>
                         </Toolbar>
                     </AppBar>
                 </Box>
