f17439d2272c2a0d8f764dfb2864201f0c55ce7e
GustavoFonsecalog <gustavofonsecajb@hotmail.com>
2023-04-28 17:28:48 -0300

    Creating login page and login/logout logic

--- cc-system/src/Routes/routes.tsx
+++ cc-system/src/Routes/routes.tsx
@@ -4,12 +4,15 @@ import { Routes, Route } from 'react-router-dom'
 
 import Home from '../pages/Home'
 import Private from '../pages/Private'
+import { RequireAuth } from '../Auth/RequireAuth';
+import { Login } from '../pages/Login';
 
 const AppRoutes = () => {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/private" element={<Private />} />
+            <Route path="/private" element={<RequireAuth><Private /></RequireAuth>} />
+            <Route path="/login" element={<Login />} />
         </Routes>
     );
 };
---
+++ cc-system/src/pages/Login/index.tsx
@@ -0,0 +1,77 @@
+import { Box, Button, Grid, Input, Typography } from '@mui/material'
+import { useState, useContext } from 'react'
+import { AuthContext } from '../../Auth/authContext'
+
+import { useNavigate } from 'react-router-dom'
+
+export const Login = () => {
+    const auth = useContext(AuthContext)
+
+    const navigate = useNavigate()
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleLogin = async () => {
+        if (email && password) {
+            const isLogged = await auth.signin(email, password)
+            if (isLogged) {
+                navigate('/private')
+            } else {
+                alert('Não deu certo')
+            }
+        }
+    }
+
+    return (
+        <Grid container sx={{
+            width: '100vw',
+            height: '100vh',
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'center',
+            alignItems: 'center',
+        }}>
+            <Grid item md={6} lg={6} xs={12}>
+                <Box sx={{
+                    background: '#620cca',
+                    padding: '100px',
+                    borderRadius: '20px'
+                }}>
+                    <Typography
+                        color={'#f8de64'}
+                        sx={{ fontWeight: 'bold' }}
+                    >
+                        Faça seu login!
+                    </Typography>
+                    <Input
+                        type="text"
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                        placeholder='email'
+                        sx={{
+                            display: 'grid',
+                            color: '#f8de64'
+                        }} />
+                    <Input
+                        type="password"
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
+                        placeholder='senha'
+                        sx={{
+                            display: 'grid',
+                            color: '#f8de64'
+                        }} />
+                    <Button
+                        onClick={handleLogin}
+                        sx={{ fontWeight: 'bold', color: '#f8de64' }}
+                    >
+                        Logar
+                    </Button>
+                </Box>
+            </Grid>
+        </Grid>
+
+
+    )
+}
